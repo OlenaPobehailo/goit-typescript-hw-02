147b8ce6e29b0d2d652650d9352183aa1f85f9b9
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-15 12:22:30 +0200

    adds solution for  baseTypes

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,11 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
+type Gender = "male" | "female"
+let myGender: Gender;
+myGender = 'female'
 
-const myGender;
+// or:
+// const myGender: Gender = 'female'
 
-export {};
+export { };
